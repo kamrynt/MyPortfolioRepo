f73f00e63c43eb170e6ea9d4bce54f94e4e803c7
kamrynt <kamryn.tate@bison.howard.edu>
2025-01-31 17:02:18 -0500

    creating html, js, and css files

---
+++ MyPortfolio/script.js
@@ -0,0 +1,4 @@
+document.getElementById('playButton').addEventListener('click', function() {
+    alert("Starting the game... (Future integration with your game engine)");
+    window.location.href = "game.html"; // Redirect to your game page
+});
